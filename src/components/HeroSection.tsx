3498565e6ff5cbc2832f368ec71a6eb103d2bd61
HemanthKumar-veeville <hemanth.kumar@veeville.com>
2024-11-14 12:53:16 +0530

    Update text

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -19,6 +19,7 @@ const StreamingPromo: React.FC<StreamingPromoProps> = ({
 }) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const outers = ["Flot Work", "Lying Racoon"];
   console.log({ isPlaying, description });
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -71,14 +72,23 @@ const StreamingPromo: React.FC<StreamingPromoProps> = ({
           {/* Main content */}
           <div className="space-y-8 text-white">
             {/* Title without glow effect */}
-            <div className="flex mx-auto w-fit items-baseline">
-              <h1 className="text-4xl md:text-6xl text-center georgia-text tracking-wide">
-                {family}
-              </h1>
-              <h1 className="text-4xl md:text-4xl text-center tracking-wide">
-                {title}
-              </h1>
-            </div>
+            {!outers?.includes(title) && (
+              <div className="flex mx-auto w-fit items-baseline">
+                <h1 className="text-4xl md:text-6xl text-center georgia-text tracking-wide">
+                  {family}
+                </h1>
+                <h1 className="text-4xl md:text-4xl text-center tracking-wide">
+                  {title}
+                </h1>
+              </div>
+            )}
+            {outers?.includes(title) && (
+              <div className="flex mx-auto w-fit items-baseline">
+                <h1 className="text-4xl  md:text-6xl text-center tracking-wide">
+                  {title}
+                </h1>
+              </div>
+            )}
             {/* Description */}
             {/* <p className="text-lg md:text-xl text-center italic max-w-2xl mx-auto leading-relaxed drop-shadow-md">
               {description}
@@ -94,13 +104,13 @@ const StreamingPromo: React.FC<StreamingPromoProps> = ({
               >
                 <button
                   type="button"
-                  className="group bg-gradient-to-r from-pink-500 to-orange-400 text-white px-8 py-3 rounded-full 
-                             hover:scale-105 active:scale-95 transition-transform duration-200 
-                             text-base font-medium shadow-lg flex items-center space-x-2"
+                  className="group bg-[#BE0005] text-white px-5 py-2.5 rounded-full 
+                 hover:scale-105 active:scale-95 transition-transform duration-200 
+                 text-sm font-medium shadow-lg flex items-center space-x-1.5"
                 >
                   <span>{buttonText}</span>
                   <svg
-                    className="w-5 h-5 transform group-hover:translate-x-1 transition-transform duration-200"
+                    className="w-4 h-4 transform group-hover:translate-x-1 transition-transform duration-200"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
